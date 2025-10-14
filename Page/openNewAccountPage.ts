110b5a2d6d48a43069edeb868033bb167dd839bc
mnkolloQAEngineer <mnkollo@gmail.com>
2025-10-13 23:33:58 -0500

    Update openNewAccountPage.ts

--- Page/openNewAccountPage.ts
+++ Page/openNewAccountPage.ts
@@ -8,13 +8,12 @@ export class OpenNewAccountPage {
     this.page = page;
   }
 
-  async openSavingsAccount(savings: string) {
-    await this.page.locator('select[id="type"]').selectOption(savings);
+  async openSavingsAccount(type: string ) {
+    await this.page.locator('select[id="type"]').selectOption(type);
     await this.page.waitForResponse(response => response.url().includes('/accounts') && response.status() === 200);
-    await this.page.locator('input[value="Open New Account"]').click();
-    expect(this.page.locator('[id="openAccountResult"] h1[class="title"]')).toHaveText('Account Opened!');
-    expect(this.page.locator('[id="openAccountResult"] p').first()).toHaveText('Congratulations, your account is now open.');
-
+    this.page.locator('input[type="button"]').click();
+    await expect(this.page.locator('[id="openAccountResult"] h1[class="title"]')).toHaveText('Account Opened!');
+    await expect(this.page.locator('[id="openAccountResult"] p').first()).toHaveText('Congratulations, your account is now open.');
   }
 
   async openCheckingAccount(){
