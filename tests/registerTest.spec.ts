4c322f0596c283c97eb5866163e64e3b151d1278
mnkolloQAEngineer <mnkollo@gmail.com>
2025-11-08 18:12:42 -0600

    if statement practice

--- tests/registerTest.spec.ts
+++ tests/registerTest.spec.ts
@@ -12,6 +12,16 @@ test.describe('Register Scenarios', () => {
   test('should register successfully with valid data', async ({ page }) => {
 
         const registerPage = new RegisterPage(page);
-        await registerPage.fillUpRegistrationForm("Michael", 'Smith', '123 main st', 'Fort Worth', 'TX', '76101', '8175551234', '123-45-6789', 'michaelsmith', 'Password123!');
+        await registerPage.fillUpRegistrationForm("Michael", 'Smith', '123 main st', 'Fort Worth', 'TX', '76101', '8175551234', '123-45-6789', 'tonytest', 'Password123!');
+  });
+  test('Verify unable to register without SSN', async ({ page }) => {
+
+        const registerPage = new RegisterPage(page);
+        await registerPage.fillUpRegistrationForm("Michael", 'Smith', '123 main st', 'Fort Worth', 'TX', '76101', '8175551234', '', 'testuser1!43892', 'Password123!');
+  });
+  test('Verify unable to register without address', async ({ page }) => {
+
+        const registerPage = new RegisterPage(page);
+        await registerPage.fillUpRegistrationForm("Michael", 'Smith', '', 'Fort Worth', 'TX', '76101', '8175551234', '123-45-6789', 'testuser1!43892', 'Password123!');
   });
 });
